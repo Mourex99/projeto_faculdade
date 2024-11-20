cb24aed33920d87d5a38b83d7ceb954369127a4d
Mateus Moura <mateusmoura.st@outlook.com>
2024-11-20 16:18:24 -0300

    Criação de novas telas

--- app/src/main/java/com/example/project_facu/FormLogin.java
+++ app/src/main/java/com/example/project_facu/FormLogin.java
@@ -1,6 +1,9 @@
 package com.example.project_facu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,50 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class FormLogin extends AppCompatActivity {
 
+    private TextView text_tela_cadastro;
+
+    private TextView text_tela_senha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_form_login);
+
+        IniciarComponentes();
+        ResetSenha();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+
+        text_tela_senha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FormLogin.this, FormSenha.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+    }
+
+    private void ResetSenha (){
+        text_tela_senha = findViewById(R.id.text_tela_senha);
+
     }
 }
